af5f9e59c88c7b681f2c5676b4929d0e9de402db
BhautikVekariya21 <bhautikvekariya1123@gmail.com>
2024-12-30 18:39:45 +0530

    Add HelloWorld.cs to demonstrate a basic Hello World application

---
+++ 01_basics/HelloWorld.cs
@@ -0,0 +1,12 @@
+// File: HelloWorld.cs
+using System;
+
+// A simple program to demonstrate a basic Hello World application.
+class HelloWorld
+{
+    static void Main()
+    {
+        // Print a message to the console
+        Console.WriteLine("Hello, World!");
+    }
+}
